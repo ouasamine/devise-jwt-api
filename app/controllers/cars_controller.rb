75428c3fa83bc4a18209ee134515c64d3385778f
Amine <ouas.amine@outlook.com>
2023-04-08 18:47:33 +0000

    Generate DB schema

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,17 @@
 class CarsController < ApplicationController
   def index
+    render json: 'Cars Index'
   end
   
   def show
+    render json: 'Car Show'
   end
 
   def create
+    render json: 'Car Create'
   end
 
   def destroy
+    render json: 'Car Delete'
   end
 end
